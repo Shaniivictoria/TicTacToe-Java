54b15817d4b75b416968309c2411a311e278ed94
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-05-16 19:31:41 -0400

    Third Commit
    I have added my imports. Below is a description of each import I have added. 
    
    AWT stands for Abstract Window Toolkit. 
    
    import java.awt.Dimension - This class wraps the width and height of a component. 
    import java.awt.color -  This class allows us to create new colors .
    import java.awt.event - This class defines classes and interfaces used for event handling.
    import java.awt.Graphics - This class allows an application to draw onto components that can be realized on various devices, or on screen images as well. 
    import java.awt.Image - This class creates a graphic content for drawing to an off screen image. 
    import java.awt.font - This class is basically the font for using Java. 
    import java.awt.Toolkit - This is a abstract class and it basically provides an interface to a platform specific details. Ex, window size, fonts that are available and printing.
    import javax.swing.JButton -This class implants a push button.
    
    javax - is a prefix used for a package of Java Standard ext. 
    
    import javax.swing.JFrame - This is inbuilt package so I can build GUI Project. 
    
    A GUI Project is a user friendly visual experience builder for Java applicants. 
    
    GUI stands for Graphical User Interfaces. 
    
    import javax.swing.JPanel - This proves space in which an application can attach to any other components.
    
    import javax.swing.ImageIcon - This import creates a uninitiated image icon. 
    import java.io.- This mean you import the class which is included in Java, Input/ Output package. 
    
    import java.util.Scanner - This is used to take input from a user for any primitive datatype. 
    
    Primitive Data Types:  Specifies the size and type of variable values, and it has no additional methods. 
    
     The following 8 are Primitive Data Types:
    
    byte - stores whole numbers from -128 to 127
    short - stores whole numbers from -32,768 to 32,767.
    int - stores whole numbers from -2,147,483,648 to 2,147,483,647.
    long - Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
    float - 	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
    double - 	Stores fractional numbers. Sufficient for storing 15 decimal digits
    boolean - stores true or false values
    char - stores a single character/letter or ASCII values

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,24 @@
+import java.awt.Dimension;
+import java.awt.color.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.font.*;
+import java.awt.Toolkit;
+import javax.swing.JButton; 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.io.*;
+import java.util.Scanner;
+
+
+
 public class TicTacToe{
     public TicTacToe(){
 
     }
     public static void main(String[]args){
-        
+
     }
 }
