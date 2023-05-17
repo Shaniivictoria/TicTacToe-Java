3f6630a745b566c60b58f96254cf811fcbc0a1c5
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-05-17 10:17:08 -0400

    Adding Colors
    In this commit I am adding color to the game board.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.color.*;
 import java.awt.event.*;
@@ -20,8 +21,21 @@ public class TicTacToe extends JPanel{
     boolean gameDOne = false;
     int winner = -1;
     int player1wins = 0, player2wins = 0;
-    iont[][]board
+    int[][]board = new int[3][3];
 
+    int loneWidth = 5;
+    int lineLength = 270;
+    int x = 15, y = 270;
+    int offset = 95;
+    int a = 0;
+    int b = 5;
+    int selX = 0;
+
+    Color turtle = new Color(0x80bdab);
+    Color orange = new Color(0xfdcb9e);
+    Color offwhite = new Color (0xf7f7f7);
+    Color darkgray = new Color(0x3f3f44);
+    
 
 
 
